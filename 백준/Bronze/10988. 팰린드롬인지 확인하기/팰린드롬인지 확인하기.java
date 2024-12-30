7caf384f72ec6e679e5c92effd89c1b27715d711
1996jessie <jessie1996@naver.com>
2024-12-30 15:20:14 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 168 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        String reversedWord = new StringBuilder(word).reverse().toString();
+        if (word.equals(reversedWord)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
