26bf780875b8b16c24d1bb40d5207c71c33406d6
1996jessie <jessie1996@naver.com>
2024-12-31 13:39:20 +0900

    [Bronze I] Title: 약수들의 합, Time: 196 ms, Memory: 18200 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            int n = sc.nextInt();
+            if (n == -1) {
+                break;
+            }
+            
+            ArrayList<Integer> divisors = new ArrayList<>();
+            int sum = 0;
+            
+            for (int i = 1; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    divisors.add(i);
+                    sum += i;
+                }
+            }
+            
+            if (sum == n) {
+                System.out.print(n + " = ");
+                for (int i = 0; i < divisors.size(); i++) {
+                    System.out.print(divisors.get(i));
+                    if (i != divisors.size() - 1) {
+                        System.out.print(" + ");
+                    }
+                }
+                System.out.println();
+            } else {
+                System.out.println(n + " is NOT perfect.");
+            }
+        }
+        
+        sc.close();
+    }
+}
