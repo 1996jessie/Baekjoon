2b532d9930c383f587c50c4b3e6ba40f8a6202ee
1996jessie <jessie1996@naver.com>
2024-12-31 15:04:31 +0900

    [Silver V] Title: 영화감독 숌, Time: 320 ms, Memory: 87168 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int count = 0;
+        int number = 666;
+        
+        while (true) {
+            if (Integer.toString(number).contains("666")) {
+                count++;
+                if (count == N) {
+                    System.out.println(number);
+                    break;
+                }
+            }
+            number++;
+        }
+    }
+}
