430a613abaf50c19869173d8ab78737299913934
1996jessie <jessie1996@naver.com>
2025-01-01 20:13:17 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 680 ms, Memory: 52248 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] points = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[1] != b[1]) {
+                    return Integer.compare(a[1], b[1]);
+                } else {
+                    return Integer.compare(a[0], b[0]);
+                }
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(points[i][0] + " " + points[i][1] + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
