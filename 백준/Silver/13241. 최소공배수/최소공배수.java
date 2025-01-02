20df674e50e9eae2d942d352cf69a8e2a33ecaf6
1996jessie <jessie1996@naver.com>
2025-01-02 18:17:05 +0900

    [Silver V] Title: 최소공배수, Time: 172 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        System.out.println(lcm(A, B));
+        scanner.close();
+    }
+    
+    private static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    private static long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+}
