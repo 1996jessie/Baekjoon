98354b20ceaa027e8fad1beade2979f30dc84876
1996jessie <jessie1996@naver.com>
2025-01-03 10:19:17 +0900

    [Bronze I] Title: 이항 계수 1, Time: 180 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(binomialCoefficient(N, K));
+    }
+
+    public static int binomialCoefficient(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    }
+}
