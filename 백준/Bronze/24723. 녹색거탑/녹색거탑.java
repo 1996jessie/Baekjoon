d6b5921969f2c18090b54439ccf836c9d997174f
1996jessie <jessie1996@naver.com>
2025-01-03 10:17:34 +0900

    [Bronze IV] Title: 녹색거탑, Time: 180 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        System.out.println((int)Math.pow(2, n));
+    }
+}
