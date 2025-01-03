e53af268f13c96faca03cad6e0a44a1c2cc18520
1996jessie <jessie1996@naver.com>
2025-01-03 10:18:12 +0900

    [Bronze III] Title: 팩토리얼, Time: 168 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(factorial(N));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
