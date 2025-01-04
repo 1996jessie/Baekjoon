161a81cff5a4ac5e9bfbe1e62abda3badee0054a
1996jessie <jessie1996@naver.com>
2025-01-04 10:44:53 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 260 ms, Memory: 21396 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] inc = new int[n];
+        int[] dec = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            inc[i] = 1;
+            dec[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    inc[i] = Math.max(inc[i], inc[j] + 1);
+                }
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (arr[i] > arr[j]) {
+                    dec[i] = Math.max(dec[i], dec[j] + 1);
+                }
+            }
+        }
+
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            maxLength = Math.max(maxLength, inc[i] + dec[i] - 1);
+        }
+
+        System.out.println(maxLength);
+        
+        sc.close();
+    }
+}
