c96cf25ad5603babe0040971480696d06f2fcf17
1996jessie <jessie1996@naver.com>
2025-01-05 21:21:53 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+        
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (K == 0) {
+                break;
+            }
+            count += K / coins[i];
+            K %= coins[i];
+        }
+
+        System.out.println(count);
+    }
+}
