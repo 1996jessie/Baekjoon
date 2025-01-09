ec5bcb0d69188422e940d990937bcf3940f138d0
1996jessie <jessie1996@naver.com>
2025-01-09 15:11:58 +0900

    [Gold IV] Title: 사이클 게임, Time: 644 ms, Memory: 123096 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+    static int[] size;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+
+        boolean cycleFound = false;
+        int cycleTurn = 0;
+
+        for (int turn = 1; turn <= m; turn++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (find(a) == find(b)) {
+                cycleFound = true;
+                cycleTurn = turn;
+                break;
+            } else {
+                union(a, b);
+            }
+        }
+
+        sb.append(cycleFound ? cycleTurn : 0);
+        System.out.print(sb.toString());
+    }
+
+    public static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (size[rootX] > size[rootY]) {
+                parent[rootY] = rootX;
+                size[rootX] += size[rootY];
+            } else {
+                parent[rootX] = rootY;
+                size[rootY] += size[rootX];
+            }
+        }
+    }
+}
