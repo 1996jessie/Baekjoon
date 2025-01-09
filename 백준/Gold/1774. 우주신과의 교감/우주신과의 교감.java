cb5d189866a64156044583b156d68ed2cdda4ae4
1996jessie <jessie1996@naver.com>
2025-01-09 15:23:36 +0900

    [Gold III] Title: 우주신과의 교감, Time: 764 ms, Memory: 49468 KB -BaekjoonHub

---
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static class Edge {
+        int from, to;
+        double weight;
+        
+        public Edge(int from, int to, double weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    static int[] parent;
+    static int[] rank;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        double[][] points = new double[n][2];
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            points[i][0] = Double.parseDouble(st.nextToken());
+            points[i][1] = Double.parseDouble(st.nextToken());
+        }
+        
+        List<Edge> edges = new ArrayList<>();
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            edges.add(new Edge(a, b, 0));
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                double dist = Math.sqrt(Math.pow(points[i][0] - points[j][0], 2) + Math.pow(points[i][1] - points[j][1], 2));
+                edges.add(new Edge(i, j, dist));
+            }
+        }
+
+        Collections.sort(edges, Comparator.comparingDouble(e -> e.weight));
+
+        parent = new int[n];
+        rank = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+
+        double result = 0;
+        int count = 0;
+        
+        for (Edge edge : edges) {
+            if (union(edge.from, edge.to)) {
+                result += edge.weight;
+                count++;
+                if (count == n - 1) {
+                    break;
+                }
+            }
+        }
+
+        System.out.printf("%.2f\n", result);
+    }
+
+    public static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public static boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+            return true;
+        }
+        return false;
+    }
+}
