cdcac8f71d9a593b5a2ec557f68a29505a530338
1996jessie <jessie1996@naver.com>
2025-01-22 15:58:30 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1572 ms, Memory: 330712 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Edge implements Comparable<Edge> {
+        int u, v, cost;
+
+        public Edge(int u, int v, int cost) {
+            this.u = u;
+            this.v = v;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    static int[] parent;
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[x] = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        ArrayList<Edge> edges = new ArrayList<>();
+        parent = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(A, B, C));
+        }
+
+        Collections.sort(edges);
+
+        int totalCost = 0;
+        int maxEdgeCost = 0;
+
+        for (Edge edge : edges) {
+            if (find(edge.u) != find(edge.v)) {
+                union(edge.u, edge.v);
+                totalCost += edge.cost;
+                maxEdgeCost = edge.cost;
+            }
+        }
+
+        System.out.println(totalCost - maxEdgeCost);
+    }
+}
