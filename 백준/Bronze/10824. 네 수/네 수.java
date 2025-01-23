cd9f953b3624ca6147f85aa0360b4bbe9172845a
1996jessie <jessie1996@naver.com>
2025-01-23 22:25:08 +0900

    [Bronze III] Title: 네 수, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        
+        long AB = Long.parseLong(input[0] + input[1]);
+        long CD = Long.parseLong(input[2] + input[3]);
+        
+        System.out.println(AB + CD);
+    }
+}
