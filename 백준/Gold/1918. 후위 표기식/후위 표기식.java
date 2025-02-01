098bdb44cf58120fd771a5fa745715d820991950
1996jessie <jessie1996@naver.com>
2025-02-01 10:57:26 +0900

    [Gold II] Title: 후위 표기식, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String infix = br.readLine();
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : infix.toCharArray()) {
+            if (Character.isLetter(c)) {
+                postfix.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c)) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+
+        System.out.println(postfix);
+    }
+
+    private static int precedence(char op) {
+        if (op == '+' || op == '-') {
+            return 1;
+        } else if (op == '*' || op == '/') {
+            return 2;
+        }
+        return 0;
+    }
+}
