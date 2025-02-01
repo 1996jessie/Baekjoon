a738a9064c18c7aceca858a59dfcac6a595c0b4b
1996jessie <jessie1996@naver.com>
2025-02-01 11:06:05 +0900

    [Bronze V] Title: 이상한 기호, Time: 108 ms, Memory: 14436 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,13 +1,14 @@
 import java.io.*;
+import java.math.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] input = br.readLine().split(" ");
-        int A = Integer.parseInt(input[0]);
-        int B = Integer.parseInt(input[1]);
-        bw.write(String.valueOf((A + B) * (A - B)));
+        BigInteger A = new BigInteger(input[0]);
+        BigInteger B = new BigInteger(input[1]);
+        bw.write(A.add(B).multiply(A.subtract(B)).toString());
         bw.flush();
         bw.close();
         br.close();
