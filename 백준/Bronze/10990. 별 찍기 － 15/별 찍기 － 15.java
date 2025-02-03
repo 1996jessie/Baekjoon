6d443abc961f86910f332a5eb51586922059f686
1996jessie <jessie1996@naver.com>
2025-02-03 21:30:53 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 252 ms, Memory: 20112 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 0) {
+                for (int j = 0; j < 2 * i - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
