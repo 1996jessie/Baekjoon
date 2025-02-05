670ba16c0aca2aef18b7366c8b2448d061c83a9f
1996jessie <jessie1996@naver.com>
2025-02-05 21:26:55 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 132 ms, Memory: 16584 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
@@ -1,13 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        int D = sc.nextInt();
+        String[] time = br.readLine().split(" ");
+        int A = Integer.parseInt(time[0]);
+        int B = Integer.parseInt(time[1]);
+        int C = Integer.parseInt(time[2]);
+        int D = Integer.parseInt(br.readLine());
         
         int currentTimeInSeconds = A * 3600 + B * 60 + C;
         int finishTimeInSeconds = currentTimeInSeconds + D;
