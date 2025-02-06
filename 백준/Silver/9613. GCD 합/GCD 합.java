9477e29674417e4145191a0d8646c25a2a503212
1996jessie <jessie1996@naver.com>
2025-02-06 22:23:07 +0900

    [Silver IV] Title: GCD 합, Time: 212 ms, Memory: 22352 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                numbers[i] = Integer.parseInt(st.nextToken());
+            }
+
+            long sum = 0;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    sum += gcd(numbers[i], numbers[j]);
+                }
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
