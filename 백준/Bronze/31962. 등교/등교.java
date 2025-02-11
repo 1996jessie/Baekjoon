0c4b357f5c765eb7ec4c53e9bae3b75ad20e338e
1996jessie <jessie1996@naver.com>
2025-02-11 22:14:43 +0900

    [Bronze IV] Title: 등교, Time: 108 ms, Memory: 14284 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31962. 등교/등교.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int latestStart = -1;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
+            
+            if (S + T <= X) {
+                latestStart = Math.max(latestStart, S);
+            }
+        }
+
+        System.out.println(latestStart);
+    }
+}
