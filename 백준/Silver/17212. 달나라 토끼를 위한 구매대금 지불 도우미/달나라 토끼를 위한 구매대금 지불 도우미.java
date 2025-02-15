5b68aded855ca75d1bc4523a0dada46759b10a02
1996jessie <jessie1996@naver.com>
2025-02-15 09:46:31 +0900

    [Silver III] Title: 달나라 토끼를 위한 구매대금 지불 도우미, Time: 120 ms, Memory: 14692 KB -BaekjoonHub

---
+++ 백준/Silver/17212. 달나라 토끼를 위한 구매대금 지불 도우미/달나라 토끼를 위한 구매대금 지불 도우미.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        int[] coins = {1, 2, 5, 7};
+
+        for (int i = 1; i <= N; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
