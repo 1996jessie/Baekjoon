4f60e2308888246d238adca621e7e17911f5d3ce
1996jessie <jessie1996@naver.com>
2025-02-18 21:20:53 +0900

    [Bronze V] Title: Арифметическая магия, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(BigInteger.valueOf(1).pow(N));
+    }
+}
