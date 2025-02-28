adb3a37919880fc7d025be6d0d9dafee50324147
1996jessie <jessie1996@naver.com>
2025-02-28 21:38:41 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = reverse(Integer.parseInt(st.nextToken()));
+        int y = reverse(Integer.parseInt(st.nextToken()));
+
+        System.out.println(reverse(x + y));
+    }
+
+    static int reverse(int num) {
+        int rev = 0;
+        while (num > 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        return rev;
+    }
+}
