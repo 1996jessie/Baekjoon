fecebd350356c7263defb59a08162f3c7193995b
1996jessie <jessie1996@naver.com>
2025-04-26 20:37:05 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 96 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int passengers = K;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            passengers -= B;
+            passengers += A;
+        }
+
+        passengers = 0;
+
+        System.out.println("비와이");
+    }
+}
