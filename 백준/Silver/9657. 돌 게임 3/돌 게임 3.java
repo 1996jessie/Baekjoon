fe0282208cc8ce2c0912efefdbe3b67cfcb7cc6f
1996jessie <jessie1996@naver.com>
2025-04-27 19:09:36 +0900

    [Silver III] Title: 돌 게임 3, Time: 104 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        boolean[] dp = new boolean[N + 5];
+        dp[1] = true;
+        dp[2] = false;
+        dp[3] = true;
+        dp[4] = true;
+
+        for (int i = 5; i <= N; i++) {
+            if (!dp[i - 1] || !dp[i - 3] || !dp[i - 4]) {
+                dp[i] = true;
+            } else {
+                dp[i] = false;
+            }
+        }
+
+        if (dp[N]) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+    }
+}
