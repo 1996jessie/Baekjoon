70c79a872082a2f07e8010576d69380e6369db2c
1996jessie <jessie1996@naver.com>
2025-05-12 20:29:43 +0900

    [Bronze II] Title: 손익분기점, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        if (B >= C) {
+            System.out.println(-1);
+        } else {
+            System.out.println(A / (C - B) + 1);
+        }
+    }
+}
