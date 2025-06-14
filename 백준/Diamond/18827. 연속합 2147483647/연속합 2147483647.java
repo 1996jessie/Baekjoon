fbd70654bc4aefa435c827bec2c859907d825bd5
1996jessie <jessie1996@naver.com>
2025-06-14 09:57:58 +0900

    [Diamond III] Title: 연속합 2147483647, Time: 400 ms, Memory: 47784 KB, Score: 20200328 point -BaekjoonHub

---
+++ 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        long maxSum = arr[0];
+        long currentSum = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        System.out.println(maxSum);
+    }
+}
