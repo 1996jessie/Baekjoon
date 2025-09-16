ed42c3517b3321d0a955480d5c2c920547f6ed9f
1996jessie <jessie1996@naver.com>
2025-09-17 07:34:06 +0900

    [Bronze III] Title: Cuboids, Time: 112 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/7581. Cuboids/Cuboids.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String line = br.readLine();
+            if (line == null) break;
+            StringTokenizer st = new StringTokenizer(line);
+            int l = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if (l == 0 && w == 0 && h == 0 && v == 0) break;
+            
+            if (l == 0) l = v / (w * h);
+            else if (w == 0) w = v / (l * h);
+            else if (h == 0) h = v / (l * w);
+            else if (v == 0) v = l * w * h;
+            
+            sb.append(l).append(" ").append(w).append(" ").append(h).append(" ").append(v).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
