2bc9191b1b3068d3d6713d7ddcb6528a0232cb74
1996jessie <jessie1996@naver.com>
2025-09-19 21:26:32 +0900

    [Bronze II] Title: 9진수, Time: 100 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/14491. 9진수/9진수.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        System.out.println(Integer.toString(T, 9));
+    }
+}
